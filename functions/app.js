c97d4f253706ccd82c81448222f107856cbec01b
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2024-05-01 21:02:19 -0300

    Edited files to work with netlify

--- functions/app.js
+++ functions/app.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("App is running..");
+});
+
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
+
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.API_TOKEN || "";
 
